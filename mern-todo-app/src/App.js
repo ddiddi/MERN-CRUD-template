d31d7f14c82f595edeed872df123267da455fbfc
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2020-09-25 16:02:27 -0700

    Add todo component pages and bootstrap components

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./create-todo.component";
+import EditTodo from "./edit-todo.component";
+import TodosList from "./todos-list.component";
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div className="container">
-        <h2>
-          MERN Stack ToDo App
-        </h2>
-      </div>
+        <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+          <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+          </div>
+        </nav>
+        <br/>
       <Route path="/" exact component={TodosList} />
       <Route path="/edit/:id" component={EditTodo} />
       <Route path="/create" component={CreateTodo} />
+      </div>
       </Router>
     )
   }
---
+++ mern-todo-app/src/edit-todo.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class EditTode extends Component {
+    render () {
+        return (
+            <div>
+                <p> Edit Todo Component </p>
+            </div>
+        )
+    }
+}
