37f26650b60836fd7c10b05abf3d533e12a19900
Dhruv Diddi <dhruv.diddi+1@gmail.com>
2020-09-25 15:42:53 -0700

    Add basic react page with Router

---
+++ mern-todo-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="container">
+        <h2>
+          MERN Stack ToDo App
+        </h2>
+      </div>
+      <Route path="/" exact component={TodosList} />
+      <Route path="/edit/:id" component={EditTodo} />
+      <Route path="/create" component={CreateTodo} />
+      </Router>
+    )
+  }
+}
+
+export default App;
